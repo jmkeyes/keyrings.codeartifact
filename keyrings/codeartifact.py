71f18406e2e9fd30ded8843c396ea51e6d3cd421
Nicholas Holden <nicholas.holden@banqora.com>
2025-04-15 11:48:44 +0100

    add ability to use aws profile env var

--- keyrings/codeartifact.py
+++ keyrings/codeartifact.py
@@ -3,6 +3,7 @@
 import re
 import boto3
 import logging
+import os
 
 from datetime import datetime
 from urllib.parse import urlparse
@@ -156,10 +157,12 @@ class CodeArtifactBackend(backend.KeyringBackend):
             name=repository_name,
         )
 
+        profile_name = config.get("profile_name") or os.getenv("AWS_PROFILE")
+        
         # Create session with any supplied configuration.
         session = boto3.Session(
             region_name=region,
-            profile_name=config.get("profile_name"),
+            profile_name=profile_name,
             aws_access_key_id=config.get("aws_access_key_id"),
             aws_secret_access_key=config.get("aws_secret_access_key"),
         )
